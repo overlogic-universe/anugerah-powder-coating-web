7828623799aab8aef2987d12ecb55c9b1eb43e35
PappaIkan <pappaUnion@gmail.com>
2024-09-29 21:36:14 +0700

    feat: create navbar & header

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,103 @@
-import { NextPage } from 'next'
-
-interface Props {}
-
-const Home: NextPage<Props> = ({}) => {
-  return <div>Home</div>
+"use client";
+import React from "react";
+import { HeroParallax } from "../ui/hero-parallax";
+import { HeaderImageConstants } from "@/constants/HeaderImageConstants";
+ 
+export default function Home() {
+  return <HeroParallax products={products} />;
 }
-
-export default Home
+export const products = [
+  {
+    title: "Moonbeam",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Cursor",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Rogue",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  
+  {
+    title: "Editorially",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Editrix AI",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Pixel Perfect",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  
+  {
+    title: "Algochurn",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Aceternity UI",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "SmartBridge",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Renderwork Studio",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  
+  {
+    title: "Creme Digital",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "Invoker Labs",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+  {
+    title: "E Free Invoice",
+    link: "#",
+    thumbnail:
+    HeaderImageConstants.image1
+  },
+];
--- src/constants/HeaderImageConstants.ts
+++ src/constants/HeaderImageConstants.ts
@@ -1,8 +1,9 @@
 // This is used to store image location constants throughout the application.
 
 // located at /public/images
-import image1 from "/images/header/images-1.jpg";
+import image1 from "/public/images/header/image-1.jpg";
+import LOGO from "/public/images/header/LOGO.png";
 
 export const HeaderImageConstants = {
-    image1,
+    LOGO, image1,
 }

5e9822380d842112a7fddd16d03e4ba1ac326c72
PappaIkan <pappaUnion@gmail.com>
2024-09-30 10:35:13 +0700

    feat: hidden 'kontak kami' when window minimize

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -46,6 +46,7 @@ const Navbar: NextPage<Props> = () => {
             </svg>
           </div>
           {/* Tombol Kontak Kami */}
+          <div className='hidden md:flex'>
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -53,6 +54,7 @@ const Navbar: NextPage<Props> = () => {
           >
             Kontak Kami
           </button>
+          </div>
         </div>
 
         {/* Menu Item Desktop */}

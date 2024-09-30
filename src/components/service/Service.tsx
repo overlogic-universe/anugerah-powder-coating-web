bcb140fda18a7e9fbc123e7d13d40d1e9a46c77b
Siswadi24 <danaperdanaputra32@gmail.com>
2024-10-01 05:21:14 +0700

    feat:added implementation services section

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -1,9 +1,37 @@
+import { GalleryImageConstants } from '@/constants/GalleryImageConstants';
 import { NextPage } from 'next'
+import ServicesCard from './ServicesCard';
 
 interface Props {}
 
 const Service: NextPage<Props> = ({}) => {
-  return <div>Apa yang Kami Tawarkan?</div>
-}
+  return <div className="w-full flex-1 p-5 md:px-5 lg:px-20 xl:px-64 2xl:px-72 items-center justify-center md:h-screen">
+    <p className="md:text-xl lg:text-2xl xl:text-5xl font-bold text-center">Apa yang kami tawarkan?</p>
+    <div className="items-center justify-center flex-col md:flex-row gap-5 mt-5 xl:mt-10">
+      <ServicesCard />
+        {/* {serviceList.map((images, title, description, index) => (
+          <serviceList key={index} title={price.title} services={price.services} />
+        ))} */}
+      </div>
+  </div>
+};
+
+export const serviceList = [
+  {
+    images: GalleryImageConstants.image1,
+    title: "Vapor Blasting + Powder Coating",
+    description: "Kombinasi kedua teknik ini memastikan permukaan sparepart anda menjadi bersih dan terlindungi dengan lapisan akhir berkualitas tinggi."
+  },
+  {
+    images: GalleryImageConstants.image2,
+    title: "Vapor Blasting",
+    description: "Membersihkan dan menghilangkan kotoran oksidasi dan pewarnaan tanpa menghilangkan logam atau mengurangi yang mendasarinya."
+  },
+  {
+    images: GalleryImageConstants.image3,
+    title: "Powder Coating",
+    description: "Teknik pewarnaan menggunakan bubuk warna khusus yang dipanaskan."
+  }
+]
 
 export default Service
---
+++ src/components/service/ServicesCard.tsx
@@ -0,0 +1,61 @@
+import { GalleryImageConstants } from "@/constants/GalleryImageConstants";
+import { NextPage } from "next";
+import Image from "next/image";
+
+interface Props {
+    image: string;
+    title: string;
+    description: string;
+}
+
+const ServicesCard: NextPage = ({ }) => {
+    // return <div className="rounded-2xl p-2 w-full md:w-80 shadow-xl bg-white h-[257px]">
+    //         <div className="bg-[#E8EDFA] p-3 rounded-2xl h-full flex justify-between flex-col">
+    //             {/* <image className="h-48 w-full object-cover rounded-t-xl" src={image} alt={title} /> */}
+    //             <Image src={GalleryImageConstants.image3} width={150} height={100} alt={"Powder Coating Pink Frame Mio"} className="rounded-2xl object-fill"></Image>
+    //         </div>
+    //         <div className="px-4 py-2">
+    //             {/* <h3 className="text-lg font-bold text-gray-900">{title}</h3> */}
+    //             {/* <p className="mt-2 text-gray-600">{description}</p> */}
+    //         </div>
+    //     </div>
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {/* Card 1 */}
+            <div className="rounded-2xl p-2 shadow-xl drop-shadow bg-white h-[257px] min-w-[250px] max-w-[350px]">
+                <div className="bg-[#E8EDFA] p-14 rounded-2xl flex justify-between flex-col">
+                    {/* <Image src={GalleryImageConstants.image3} width={150} height={100} alt={"Powder Coating Pink Frame Mio"} className="rounded-t-2xl object-cover text-center"></Image> */}
+                </div>
+                <div className="px-4 py-2">
+                    {/* <h3 className="text-lg font-bold text-gray-900">{title}</h3> */}
+                    {/* <p className="mt-2 text-gray-600">{description}</p> */}
+                </div>
+            </div>
+
+            {/* Card 2 */}
+            <div className="rounded-2xl p-2 shadow-xl drop-shadow bg-white h-[257px] min-w-[250px] max-w-[350px]">
+                <div className="bg-[#E8EDFA] p-14 rounded-2xl flex justify-between flex-col">
+                    {/* <Image src={GalleryImageConstants.image3} width={150} height={100} alt={"Powder Coating Pink Frame Mio"} className="rounded-t-2xl object-cover text-center"></Image> */}
+                </div>
+                <div className="px-4 py-2">
+                    {/* <h3 className="text-lg font-bold text-gray-900">{title}</h3> */}
+                    {/* <p className="mt-2 text-gray-600">{description}</p> */}
+                </div>
+            </div>
+
+            {/* Card 3 */}
+            <div className="rounded-2xl p-2 shadow-xl drop-shadow bg-white h-[257px] min-w-[250px] max-w-[350px]">
+                <div className="bg-[#E8EDFA] p-14 rounded-2xl flex justify-between flex-col">
+                    {/* <Image src={GalleryImageConstants.image3} width={150} height={100} alt={"Powder Coating Pink Frame Mio"} className="rounded-t-2xl object-cover text-center"></Image> */}
+                </div>
+                <div className="px-4 py-2">
+                    {/* <h3 className="text-lg font-bold text-gray-900">{title}</h3> */}
+                    {/* <p className="mt-2 text-gray-600">{description}</p> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServicesCard;
